01a7767dd64c6182b0d9cecbc2e6b046e1034152
Nickolay <enhas8137@gmail.com>
2017-12-09 15:57:25 +0300

    Added a local server and watch tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,14 @@
 const gulp = require("gulp");
 const del = require("del");
 const newer = require("gulp-newer");
+const connect = require('gulp-connect');
 
 
 gulp.task("to_public", function () {
 	return gulp.src("dev/**")
 		.pipe(newer("public"))
-		.pipe(gulp.dest("public"));
+		.pipe(gulp.dest("public"))
+		.pipe(connect.reload());
 });
 
 gulp.task("clean", function () {
@@ -16,3 +18,16 @@ gulp.task("clean", function () {
 });
 
 gulp.task("build", ["clean", "to_public"]);
+
+gulp.task("connect", function() {
+	connect.server({
+		root: "public",
+		livereload: true
+	});
+});
+
+gulp.task("start", ["build", "connect", "watch"]);
+
+gulp.task("watch", function () {
+	gulp.watch(["dev/**"], ['build']);
+});
