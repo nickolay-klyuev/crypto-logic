33f730997f7edb9d034c47e5c5982f1fa533f4b0
Nickolay <enhas8137@gmail.com>
2017-12-09 15:33:51 +0300

    A basic tuning for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,18 @@
 "use strict";
 
 const gulp = require("gulp");
+const del = require("del");
+const newer = require("gulp-newer");
+
+
+gulp.task("to_public", function () {
+	return gulp.src("dev/**")
+		.pipe(newer("public"))
+		.pipe(gulp.dest("public"));
+});
+
+gulp.task("clean", function () {
+	return del("public");
+});
+
+gulp.task("build", ["clean", "to_public"]);
